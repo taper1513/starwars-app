f7c8de070568c7adf49329558a772932109837cf
tarcisio <taper1513@gmail.com>
2025-04-29 20:57:26 -0300

    applying responsiveness to divisor

--- frontend/src/components/SearchResults.tsx
+++ frontend/src/components/SearchResults.tsx
@@ -20,7 +20,7 @@ type Result = {
     return (
       <div className="bg-white shadow p-8 lg:w-[582px] h-[582px] overflow-y-auto">
         <h2 className="text-2xl font-bold text-black mb-6">Results</h2>
-        <div className="h-[1px] w-[522px] bg-[#c4c4c4]"></div>
+        <div className="h-[1px] lg:w-[522px] bg-[#c4c4c4] sm:w-full"></div>
         {isLoading ? (
           <div className="flex items-center justify-center h-[450px]">
             <p className="font-montserrat text-center text-[14px] font-bold text-[#c4c4c4]">
